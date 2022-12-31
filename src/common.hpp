0f21663ae7d79e3efada197534de06a7a9d0796f
vadym <vsukolen@anaqua.com>
2022-12-31 13:32:56 +0800

    fix windows link issues

--- src/common.hpp
+++ src/common.hpp
@@ -8,6 +8,20 @@
 #include <string_view>
 #include <vector>
 
-std::vector<std::string_view> splitLines(const std::string_view &input);
+std::vector<std::string_view> splitLines(const std::string_view &input)
+{
+  std::vector<std::string_view> lines;
+  auto start = 0;
+  auto index = 1;
+  for (const auto &item : input) {
+    if (item == '\n') {
+      lines.emplace_back(input.begin() + start, input.begin() + index - 1);
+      start = index;
+    }
+    ++index;
+  }
+  if (start != index - 1) { lines.emplace_back(input.begin() + start, input.begin() + index - 1); }
+  return lines;
+}
 
 #endif// AOC_2022_CPP_COMMON_HPP
